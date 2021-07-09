cdd7eb96d8cefb3cd5b236d176e71e7a40e56d0f
Guido747 <60378893+guidorosman@users.noreply.github.com>
2021-07-09 12:03:26 -0300

    Update db.php

--- db.php
+++ db.php
@@ -3,9 +3,9 @@
 session_start();
 
 $conn = mysqli_connect(
-    'localhost',
-    'root',
-    '',
-    'php_mysql_crud'
+    '/* host */',
+    '/* user db */',
+    ' /* password db */',
+    '/* name db */'
 );
 ?>
